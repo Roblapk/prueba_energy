64dd51c47ecd7d2513209d1d7798d7a1dbf8b71e
Rogelio Bernal <robs_apk@hotmail.com>
2024-05-24 17:58:03 -0600

    all model

---
+++ test_energy/Model/AllModel.swift
@@ -0,0 +1,17 @@
+//
+//  AllModel.swift
+//  test_energy
+//
+//  Created by Rogelio Bernal on 24/05/24.
+//
+
+import Foundation
+
+struct AllModel: Hashable, Codable{
+    
+    let userId: Int
+    let id: Int
+    let title: String
+    let completed: Bool
+    
+}
