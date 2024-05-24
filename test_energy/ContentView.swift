abcc4548b412d184fac98cb3c3cea70bf25d047c
Rogelio Bernal <robs_apk@hotmail.com>
2024-05-24 12:55:25 -0600

    create project

---
+++ test_energy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test_energy
+//
+//  Created by Rogelio Bernal on 24/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+/*struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}*/
---
+++ test_energy/test_energyApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_energyApp.swift
+//  test_energy
+//
+//  Created by Rogelio Bernal on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct test_energyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
